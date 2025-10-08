f9f4376f56644a216230099e44928a849e3bc031
Overloadedq <artleonid20022509@gmail.com>
2025-10-08 10:28:55 +0300

    -- Реализованы методы в классе InMemorySubscriberRepo

--- src/main/java/org/billing/io/InMemorySubscriberRepo.java
+++ src/main/java/org/billing/io/InMemorySubscriberRepo.java
@@ -3,30 +3,41 @@ package org.billing.io;
 import org.billing.domain.Subscriber;
 import org.billing.repo.SubscriberRepository;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class InMemorySubscriberRepo implements SubscriberRepository {
-    private Map<String, Subscriber> subscribers;
+    private final Map<String, Subscriber> subscribers;
     public InMemorySubscriberRepo() {
         subscribers = new HashMap<>();
     }
 
 
+
     @Override
     public Optional<Subscriber> findByPhone(String phoneNumber) {
-        return Optional.empty();
+        if(subscribers.isEmpty()) {
+            return Optional.empty();
+        }
+        Subscriber subscriber = subscribers.get(phoneNumber);
+        return Optional.ofNullable(subscriber);
+
     }
 
     @Override
     public void save(Subscriber subscriber) {
+        if(subscriber==null) {
+            throw new IllegalArgumentException("Subscriber is null");
+        }
+        String phoneNumber = subscriber.getPhoneNumber();
+        if(phoneNumber==null||phoneNumber.isEmpty()) {
+            throw new IllegalArgumentException("Phone number is null or empty");
+        }
+        subscribers.put(phoneNumber, subscriber);
 
     }
 
     @Override
     public List<Subscriber> findAll() {
-        return List.of();
+        return new ArrayList<>(subscribers.values());
     }
 }
